a54f115588c1e8933be3c5b18802d41aface29ed
grisha <griiisha2111@gmail.com>
2026-05-17 03:26:45 +0500

    jwt implementation forgot

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-security:4.1.0-RC1")
+    implementation("jakarta.annotation:jakarta.annotation-api:3.0.0")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.13.0")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
